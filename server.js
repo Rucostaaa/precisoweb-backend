05878cfeedafdcf1774bfaf375d88deec4dd5c3b
RubenCostaa <rubenfilipecosta.maths@gmail.com>
2025-04-14 20:56:32 +0100

    commit

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const allowedOrigins = [
-  'https://precisoweb-frontend.onrender.com/',
+  'http://precisoweb-frontend.onrender.com','https://precisoweb-frontend.onrender.com','precisoweb-frontend.onrender.com'
 ];
 
 app.use(cors({
